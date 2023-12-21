db4ffb0b4a32041d2bb5bc4dc4e31a59452647ac
Mohamed Raouf <mohamedabdelraouf122@gmail.com>
2023-12-21 09:22:28 +0200

    Store aGrid using vectors

---
+++ UDACITY-Cpp/Store_Grid/src/StoreGrid.cpp
@@ -0,0 +1,22 @@
+//============================================================================
+// Name        : FirstProject_C++.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	vector<vector<int>> vec(5);
+	vec[0]={0,1,0,0,0,0};
+	vec[1]={0,1,0,0,0,0};
+	vec[2]={0,1,0,0,0,0};
+	vec[3]={0,1,0,0,0,0};
+	vec[4]={0,0,0,0,1,0};
+
+	return 0;
+}
